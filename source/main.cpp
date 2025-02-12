4c401aefefb7f74c71a341b3bb552c2b116034bf
veortiz1 <veortiz1@asu.edu>
2025-02-11 21:56:34 -0700

    include all dependices

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 using namespace std;
-
+#include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
 
 
 int main(int argc,char* argv[]){
+
+    string query=argv[1];
+
+    string filename=argv[2];
+
+    ifstream inputfile(filename);
+
+    if(inputfile){
+        cout<<"True!";
+    }
+    else{
+        return 0;
+    }
+
+    string file_line;
+
+   vector<string> lines;
+    while(getline(inputfile,file_line)){
+        lines.push_back(file_line);
+
+    }
+
+
+
+
+    
+
+
+
+
 
 
     
