db88c11ca3982df7dbf20a4659c7e7d358656f6b
veortiz1 <veortiz1@asu.edu>
2025-02-11 22:10:11 -0700

    added first file call

--- source/main.cpp
+++ source/main.cpp
@@ -26,9 +26,24 @@ int main(int argc,char* argv[]){
    vector<string> lines;
     while(getline(inputfile,file_line)){
         lines.push_back(file_line);
-
     }
 
+     istringstream query_spaces(query);
+
+     vector <string> tokens;
+
+     string current_word;
+
+     while(query_spaces >> current_word){
+        tokens.push_back(current_word);
+
+     }
+
+     if(tokens[0]=="Add"){
+        cout<<"adddd";
+     }
+
+     
 
 
 
